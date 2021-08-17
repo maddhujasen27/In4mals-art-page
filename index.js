a56711250d5b485e1b55d59fd4f92a66348a7109
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-08-17 21:42:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,8 @@
 	//setting up the listener
 	bars.addEventListener("click", barClicked, false);
 
+	//setting up the clicked Effect
+	function barClicked() {
+		bars.classList.toggle('active');
+		nav.classList.toggle('visible');
+	}
